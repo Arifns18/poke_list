4dcb564a65532aec6bbe29c01bdaff044d85d04d
arifns <nurarifns18@gmail.com>
2025-08-22 10:32:38 +0800

    loading + list with picture

--- poke_list/src/components/PokeApi.vue
+++ poke_list/src/components/PokeApi.vue
@@ -6,27 +6,50 @@ export default {
 
   data () {
     return {
-      list:[]
+      list:[],
+      loading: true,
     }
   },
 
   async mounted() {
+    try {
+    //Get list of pokemon
     let response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100&offset=20")
-    console.log(response)
     this.list = response.data.results
-    console.log(this.list)
+    
+    //Fetch details
+    let details = await Promise.all(
+      response.data.results.map((p) => axios.get(p.url).then((r) => r.data))
+    )
+    this.list = details
+
+    } catch (err) {
+      console.error("Pokémon fetch error:", err);
+    } finally {
+      this.loading = false;
+    }
   }
 
 }
 </script>
 
 <template>
-  <h1>Pokemon List</h1>
-  <ol>
-    <li v-for = "item in list" :key="item.name">
-    {{item.name}}
-    {{item.url}}
-    </li>
-  </ol>
+  <div v-if="loading">Loading Pokémon...</div>
+  <div v-else>
+    <h1>Pokémon List</h1>
+    <ol>
+      <li v-for = "pokemon in list" :key="pokemon.name">
+        
+        <img
+          :src="pokemon.sprites.front_default"
+          :alt="pokemon.name"
+          class="mx-auto"
+        />
+        <div class="container">
+          <h4><b>J{{pokemon.name}}</b></h4>
+        </div>
+      </li>
+    </ol>
+</div>
 </template>
 

d13fa07e04b01367049b7d16863a2721a58fe6c5
Muhammad Nur Arif <73942198+Arifns18@users.noreply.github.com>
2025-08-25 02:57:18 +0800

    Add pinia store

--- poke_list/src/components/PokemonList.vue
+++ poke_list/src/components/PokemonList.vue
@@ -1,73 +1,55 @@
-<script>
-import axios from "axios"
+<script setup>
+import { onMounted, computed, ref } from "vue";
+import { usePokemonStore } from "../stores/pokemonList";
 
-export default {
-  name: 'App',
+const store = usePokemonStore();
+const search = ref("");
 
-  data () {
-    return {
-      list:[],
-      search: "",
-      loading: true,
-    }
-  },
-  computed: {
-    filteredPokemons() {
-      // return pokemons that match search input
-      return this.list.filter((p) =>
-        p.name.toLowerCase().includes(this.search.toLowerCase())
-      );
-    },
-  },
-
-  async mounted() {
-    try {
-    //Get list of pokemon
-    let response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100&offset=20")
-    this.list = response.data.results
-    
-    //Fetch details
-    let details = await Promise.all(
-      response.data.results.map((p) => axios.get(p.url).then((r) => r.data))
-    )
-    this.list = details
-
-    } catch (err) {
-      console.error("Pokémon fetch error:", err);
-    } finally {
-      this.loading = false;
-    }
+// Load Pokémon list when component mounts
+onMounted(() => {
+  if (!store.list.length) {
+    store.fetchList(100);
   }
+});
 
-}
+// Filtered list based on search
+const filteredPokemons = computed(() =>
+  store.list.filter((p) =>
+    p.name.toLowerCase().includes(search.value.toLowerCase())
+  )
+);
 </script>
 
 <template>
-  <h1>Pokémon List</h1>
+    <h1 class="text-2xl font-bold mb-4">Pokémon List</h1>
+    <div class="p-2 rounded w- mb-4">
+          <input
+            v-model="search"
+            type="text"
+            class="form-control w-100"
+            placeholder="Search Pokémon..."
+          />
+    </div>
+    
+    <div v-if="store.loading">Loading Pokémon...</div>
+    <div v-else
+      v-for="pokemon in filteredPokemons" 
+      :key="pokemon.id"
+      class="row"
+    >
+      <router-link 
+      class="card text-center" 
+      :to="`/pokemon/${pokemon.id}`"
+      >
+        <h5 class="card-header">{{ pokemon.name }}</h5>
+        <div class="card-body">
+          <img
+          :src="pokemon.sprites.front_default"
+          :alt="pokemon.name"
+          class="mx-auto w-100"
+          />
+        </div>
+      </router-link>
+    </div>
 
-    <!-- Search Input -->
-    <input
-      v-model="search"
-      type="text"
-      placeholder="Search Pokémon..."
-      class="border p-2 rounded w-full mb-4"
-    />
-  <div v-if="loading">Loading Pokémon...</div>
-  <div v-else>
-    <router-link 
-    v-for = "pokemon in filteredPokemons" 
-    :key="pokemon.id"
-    :to="`/pokemon/${pokemon.id}`">
-      
-      <img
-        :src="pokemon.sprites.front_default"
-        :alt="pokemon.name"
-        class="mx-auto"
-      />
-      <div class="container">
-        <h4><b>{{pokemon.name}}</b></h4>
-      </div>
-    </router-link>
-</div>
 </template>
-
